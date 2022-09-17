a01217c2040405c8ce89a9fcded36f13239097de
LamSamChan <chan94170@gmail.com>
2022-09-17 21:54:40 +0700

    alo alo

--- QLK/QLK/obj/Debug/net6.0-windows/App.g.cs
+++ QLK/QLK/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace QLK {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace QLK {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.4.0")]
         public static void Main() {
             QLK.App app = new QLK.App();
             app.InitializeComponent();
